97e04a7299e9ba4b9f6ef88f066177e2fe1f6c51
Matt Needham <matthew.m.needham@gmail.com>
2018-10-02 08:45:39 -0400

    create service to load nba_teams json file

---
+++ src/app/team.ts
@@ -0,0 +1,8 @@
+export class Team {
+	team: string;
+	city: string,
+	mascot: string,
+	conference: string;
+	color1: string;
+	color2: string,
+}
---
+++ src/app/teams.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamsService {
+
+  constructor(private http: HttpClient) {
+  	this.getJSON().subscribe(data => {
+  		console.log(data)
+  	});
+  }
+
+  public getJSON(): Observable<any> {
+  	return this.http.get('./assets/nba_teams.json')
+  }
+}

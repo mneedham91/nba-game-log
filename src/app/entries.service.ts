23f1b1b355fb097cbe84569c1c6e446916eb5fdf
Matt Needham <matthew.m.needham@gmail.com>
2018-10-04 12:33:01 -0400

    typo

--- src/app/entries.service.ts
+++ src/app/entries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Entry } from './entry';
-import { GlobalService } from './global-service'
+import { GlobalService } from './global.service'
 
 @Injectable({
   providedIn: 'root'

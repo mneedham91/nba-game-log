8b41818008e42c92b58c46d8474765ede6f2d843
Matt Needham <matthew.m.needham@gmail.com>
2018-10-04 16:18:36 -0400

    experimenting with login method of authService

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'

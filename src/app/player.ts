78b0cab0d7344bcdf2c23ec3ecd6f86e54dfd826
Matt Needham <matthew.m.needham@gmail.com>
2018-10-05 17:17:03 -0400

    created player class

---
+++ src/app/player.ts
@@ -0,0 +1,7 @@
+export class Player {
+	_id: string;
+	nba_id: string;
+	first_name: string;
+	last_name: string;
+	team: string;
+}
--- src/app/players.service.ts
+++ src/app/players.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Player } from './player';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  //baseUrl: string = 'http://localhost:3000';
+  baseUrl: string = '/api/v1';
+  headers = new HttpHeaders({'Authorization': 'Bearer ' + localStorage.getItem('token')});
+  options: {} = { headers: this.headers };
+
+  getPlayers() {
+  	return this.http.get<Player[]>(this.baseUrl + '/player');
+  }
+
+  getPlayerById(_id: string) {
+  	return this.http.get<Player>(this.baseUrl + '/player/' + _id);
+  }
+
+  getTeam(team: string) {
+  	return this.http.get<Player[]>(this.baseUrl + '/team/' + team);
+  }
 }

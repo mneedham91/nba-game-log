f860c57e7400b7c45e66fd2444c86a6cc6645e94
Matt Needham <matthew.m.needham@gmail.com>
2018-10-05 10:15:35 -0400

    adding email validation to registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -18,7 +18,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
 
   	this.registerForm = this.formBuilder.group({
-  		email: ['', Validators.required],
+  		email: ['', [Validators.required, Validators.email]],
   		password: ['', Validators.required]
   	})
   }

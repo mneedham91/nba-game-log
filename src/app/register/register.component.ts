bfe3ba4064bb20f23f8065d3579c3724c6243206
Matt Needham <matthew.m.needham@gmail.com>
2018-10-05 11:12:44 -0400

    correct import

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { UsersService } from '../users.service';
-import { PasswordValidation } from './password-validation';
+import { PasswordValidation } from '../password-validation';
 
 @Component({
   selector: 'app-register',

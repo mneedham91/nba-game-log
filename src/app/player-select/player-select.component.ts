2cc0be438fe08cf07e3f20f530845a0b82405815
Matt Needham <matthew.m.needham@gmail.com>
2018-10-06 08:57:27 -0400

    subscribe to team roster after team name change

--- src/app/player-select/player-select.component.ts
+++ src/app/player-select/player-select.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
-import { TeamsService } from '../teams.service';
+import { PlayersService } from '../players.service';
 import { Player } from '../player';
 import { FormGroup, ControlContainer } from '@angular/forms';
 
@@ -12,10 +12,13 @@ export class PlayerSelectComponent implements OnChanges {
   @Input() team: string;
   roster: Player[];
 
-  constructor(private teamsService: TeamsService) { }
+  constructor(private playersService: PlayersService) { }
 
   ngOnChanges(changes: SimpleChanges) {
   	console.log(changes);
+  	this.playersService.getTeam(this.team).subscribe( data => {
+  		this.roster = data;
+  	});
   }
 
 }

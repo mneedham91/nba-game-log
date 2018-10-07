1ea816d34098e949b93b88eb9aac1a60e3f918f5
Matt Needham <matthew.m.needham@gmail.com>
2018-10-07 18:13:39 -0400

    attempting to figure out mongoose aggregate function

--- module.factory.js
+++ module.factory.js
@@ -93,7 +93,7 @@ var Factory = function(Schema,mongoose) {
 	}
 
 	this.getGamesCount = function(id,res) {
-		this.Entry.aggregate( [{ $match: {userid: id} }, {total: {$sum: 1}}], 
+		this.Entry.aggregate( [ { $match: {userid: id} }, { $group: {$sum: 1}}], 
 			function(error, output) {
 				return res.json(output);
 			}

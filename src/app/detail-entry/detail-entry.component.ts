e2afce158f651a08aa35dceff04850b02cbfdb47
Matt Needham <matthew.m.needham@gmail.com>
2018-10-07 14:38:06 -0400

    adding tagged players to detail-entry

--- src/app/detail-entry/detail-entry.component.ts
+++ src/app/detail-entry/detail-entry.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { EntriesService } from '../entries.service';
+import { TagsService } from '../tags.service';
+import { PlayersService } from '../players.service';
 import { UsersService } from '../users.service';
 import { Entry } from '../entry';
+import { Player } from '../player';
 import { User } from '../user';
+import { Tag } from '../tag';
 
 @Component({
   selector: 'app-detail-entry',
@@ -15,12 +19,15 @@ export class DetailEntryComponent implements OnInit {
   entry_user: User;
   private sub: any;
   entry: Entry;
+  players: Player[] = [];
 
   constructor(
     private route: ActivatedRoute, 
     private router: Router, 
     private entriesService: EntriesService,
-    private usersService: UsersService) { }
+    private usersService: UsersService,
+    private tagsService: TagsService,
+    private playersService: PlayersService) { }
 
   ngOnInit() {
   	this.sub = this.route.params.subscribe(params => {
@@ -29,6 +36,16 @@ export class DetailEntryComponent implements OnInit {
     this.entriesService.getEntryById(this.id)
       .subscribe( data => {
         this.entry = data;
+        this.tagsService.getTagsByEntry(this.id)
+          .subscribe( data => {
+            let tags: Tag[] = data;
+            for (let i = 0; i < tags.length; i++) {
+              this.playersService.getPlayerById(tags[i].playerid)
+                .subscribe( data => {
+                  this.players.push(data);
+                });
+            }
+          });
         this.usersService.getUserById(this.entry.userid)
           .subscribe( data => {
             this.entry_user = data;

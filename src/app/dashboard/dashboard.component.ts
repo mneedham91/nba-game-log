b0bcb8825328cfbca8c22a288263e929c1c55303
Matt Needham <matthew.m.needham@gmail.com>
2018-10-15 16:48:54 -0400

    add ability for logged in users to edit themselves from dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  loggedIn: boolean;
+  userid: string;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) {
+  	this.authService.isLoggedIn.subscribe( value => {
+		  this.loggedIn = value;
+	  }); 
+    this.authService.userid.subscribe( value => {
+      this.userid = value;
+    });
+  	}
 
   ngOnInit() {
   }

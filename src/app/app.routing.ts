1f2b0d3874379895d9c05b1a39f0649d9626dc55
Matt Needham <matthew.m.needham@gmail.com>
2018-10-28 14:05:06 -0400

    add navigation to forgot-password

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { AdminComponent } from './admin/admin.component';
 import { EditEntryComponent } from './edit-entry/edit-entry.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { EntriesComponent } from './entries/entries.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { DetailEntryComponent } from './detail-entry/detail-entry.component';
 import { DetailUserComponent } from './detail-user/detail-user.component';
 import { LoginComponent } from './login/login.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'admin', component: AdminComponent },
   { path: 'entries', component: EntriesComponent },
   { path: 'edit-entry/:id', component: EditEntryComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'detail-entry/:id', component: DetailEntryComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'detail-user/:id', component: DetailUserComponent},
